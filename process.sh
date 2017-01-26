c5776744b9f59db0fe2e67b28cf67952c6aac339
mmxfan <mmxfan@qq.com>
2017-01-26 10:22:46 +0800

    add and clean

--- process.sh
+++ process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wavfile="speech.wav"
-if [ -f $wavfile ]; then
-        du -h $wavfile
+# wavfile="speech.wav"
+if [ -f $1 ]; then
+    du -h $1 | cut -f1
 else
 	echo "$0 received: $1"
 fi
